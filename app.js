e46ac90daa08f9ab78cbf86cbae671fea8205d2b
Balakrishna Avulapati <bavulapati@gmail.com>
2020-02-19 00:34:57 +0530

    import data and connect to mongo db

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const dbUrl = "mongodb://localhost:27017/bookAPI";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log('Connected to mongodb');
+}); 
 
 bookRouter.route('/books')
   .get((req, res) => {
