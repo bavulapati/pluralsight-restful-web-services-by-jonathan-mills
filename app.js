5af7799a5b9bbabeea3ff23cce75fbe954adfafe
Balakrishna Avulapati <bavulapati@gmail.com>
2020-02-21 20:53:32 +0530

    Save data and inject class

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
-const bookRouter = express.Router();
-const port = process.env.PORT || 3000;
-const dbUrl = 'mongodb://localhost:27017/bookAPI';
+const bodyParser = require('body-parser');
 const Book = require('./models/bookModel');
 
+const app = express();
+const bookRouter = require('./routes/bookRouter')(Book);
+const port = process.env.PORT || 3000;
+const dbUrl = 'mongodb://localhost:27017/bookAPI';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 mongoose.connect(dbUrl);
-
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-
-bookRouter.route('/book/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 
 app.use('/api', bookRouter);
 
